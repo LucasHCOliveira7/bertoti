0ea1e9ef399bb1970cbd148e79b9737d53fe9afa
Lucas Henrique Costa de Oliveira <lucascosta13abc@gmail.com>
2024-11-24 20:11:27 -0300

    padrões de projeto - composite

---
+++ padroes-de-projetos/Composite/src/main/java/org/example/ArquivoComponent.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public interface ArquivoComponent {
+    void mostrarNome();
+    void adicionar(ArquivoComponent componente);
+    void remover(ArquivoComponent componente);
+}
